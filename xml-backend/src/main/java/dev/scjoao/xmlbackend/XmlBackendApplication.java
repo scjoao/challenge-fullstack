c37b4e854ba4a2f3515ca950d95c9cccfd05a7a9
João Carlos <joao.carlos_25@hotmail.com>
2023-09-14 11:04:06 -0300

    Initial commit

---
+++ xml-backend/src/main/java/dev/scjoao/xmlbackend/XmlBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.scjoao.xmlbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmlBackendApplication.class, args);
+	}
+
+}
---
+++ xml-backend/src/test/java/dev/scjoao/xmlbackend/XmlBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.scjoao.xmlbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmlBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
